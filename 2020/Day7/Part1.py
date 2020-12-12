8d6c8f5c76438be471d06a4144e6ab44de6bc748
hmooms <hmooms@hotmail.com>
2020-12-13 00:19:59 +0100

    Preparing day 7

---
+++ 2020/Day7/Part1.py
@@ -0,0 +1,17 @@
+""" 
+    small idea: 
+    
+    bags [
+        {'color': "", 'can_contain': ['color']},
+    ]
+
+    if bag cant contain anything dont add
+
+    start with shiny gold
+
+    for bag in bags
+
+    checked_bags ["color", ..]
+    can_contain ["color", ..]
+
+ """
