b531967303a2faa79d074cbf079e8bdf5746f5e9
hmooms <99046763@mydavinci.nl>
2020-12-13 10:41:22 +0100

    made a small start on day 7

--- 2020/Day7/Part1.py
+++ 2020/Day7/Part1.py
@@ -15,3 +15,19 @@
     can_contain ["color", ..]
 
  """
+
+input_file = open(r"AdventOfCode/2020/Day7/input.txt", "r")
+
+bags = []
+
+for line in input_file:
+    bag = {}
+    if line.find("contain no other bags."):
+        continue
+    line = line.replace("\n", "")
+    split_line = line.split(" ")
+    bag = {'color': f"{split_line[0]} {split_line[1]}"}
+
+    bags.append(bag)
+
+print(bags)
